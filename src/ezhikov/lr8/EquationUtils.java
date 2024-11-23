6d2f3335c0d2c4f5775d4ebead57a5aa077e0fc7
Pavel Ezhikov <Pavel.Ezhikov@urfu.me>
2024-11-23 15:32:01 +0500

    Add private constructor for utility class lr8

--- src/ezhikov/lr8/EquationUtils.java
+++ src/ezhikov/lr8/EquationUtils.java
@@ -7,11 +7,14 @@ import java.util.List;
  * Утилиты для работы с уравнениями
  */
 public class EquationUtils {
+    private EquationUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
     /**
      * Обобщенный статический метод для сортировки списка уравнений
      *
      * @param equations список уравнений
-     * @param <T>       тип уравнения, реализующего Comparable
      */
     public static <T extends Comparable<T>> void sortEquations(List<T> equations) {
         Collections.sort(equations);
